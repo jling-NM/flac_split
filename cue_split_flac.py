ec7f9d42169a113bbf7dc5f30b92a9febbd55f81
josef ling <jling@swcp.com>
2015-12-10 23:14:42 -0700

    Added all tags and picture for tracks

--- cue_split_flac.py
+++ cue_split_flac.py
@@ -57,6 +57,7 @@ class cuesheet:
         self.cue_file   = cue_file    
         self.f_path     = os.path.split(cue_file)[0]
         self.audio_file = ""
+        #self.cover_image= None
         self.tags       = {}
         self.tracks     = []
         self.t          = track(00)
@@ -111,18 +112,47 @@ class cuesheet:
     def __str__(self):
         return 'CUESHEET:{0}'.format(self.cue_file)
 
-
+    def img_parse(self,new_image_out_path):
+        """
+        xxx
+        """
+        i_file = os.path.join(self.f_path,'cover.jpg')
+        if os.path.isfile( i_file ):
+            
+            from PIL import Image
+            i_img = Image.open(i_file)
+            #if i_img.size[0] > 500:
+            resize_ratio = 500/i_img.size[0]
+            resize_width = int(resize_ratio*i_img.size[0])
+            resize_height= int(resize_ratio*i_img.size[1])
+            resized_img = i_img.resize( (resize_width, resize_height), Image.ANTIALIAS )
+            resized_img.save(os.path.join(new_image_out_path,'folder.jpg'))
+            #self.cover_image = os.path.join(self.f_path,'folder.jpg')
+                
+            #else:
+            #    self.cover_image = default_cover_image
+                
+        
+        
 def main(argv):
     
-    if not os.path.isfile(argv[1]):
-        exit("Cannot file cue file: " + argv[1])
+    i_cue_file = argv[1]
+    o_new_files= argv[2]
+    
+    
+    if not os.path.isfile(i_cue_file):
+        exit("Cannot file cue file: " + i_cue_file)
     else:
-        cs = cuesheet(argv[1])     
+        cs = cuesheet(i_cue_file)     
         cs.cue_parse()
         
-        new_f_path = os.path.join(argv[2],cs.tags['ALBUM'] )
+        
+        new_f_path = os.path.join(o_new_files,cs.tags['ALBUM'] )
         if not os.path.exists(new_f_path):
             os.makedirs(new_f_path)
+        
+        cs.img_parse(new_f_path)
+        
         
         # TODO: if cover.jpg put that into cuesheet object or just directly into cmdline 
         # however, i'll first have to check for resolution on that cover.jpg and reduce it to 500
@@ -130,37 +160,37 @@ def main(argv):
         
         
         # process each cue track
-        # loop using an index so we can get data from previous or next track 
-        
+        # loop using an index so we can get data from previous or next track         
         for track_indx in range(len(cs.tracks)):
-            
-            # initiate command by dumping out all tags
-            cmd = 'flac ' + ' '.join("--tag={!s}={!r}".format(key,val) for (key,val) in cs.tracks[track_indx].tags.items() )
-           
-            if track_indx == 0:
-                cmd = cmd + ' --until={{{0}}}'.format(cs.tracks[track_indx+1].seek_index)
+                        
+            # check for existing first 
+            new_file = '{0}{1}track{2}.flac'.format(new_f_path,os.path.sep,cs.tracks[track_indx].num)
+            if not os.path.isfile(new_file):
+
+                # initiate command by dumping out all tags
+                cmd = 'flac --silent --compression-level-6 ' + ' '.join("--tag={!s}={!r}".format(key,val) for (key,val) in cs.tracks[track_indx].tags.items() )
+               
+                if track_indx == 0:
+                    cmd = cmd + ' --until={0}'.format(cs.tracks[track_indx+1].seek_index)
+                    
+                elif track_indx == len(cs.tracks)-1:    
+                    cmd = cmd + ' --skip={0}'.format(cs.tracks[track_indx].seek_index)
+                    
+                else:
+                    cmd = cmd + ' --skip={0} --until={1}'.format(cs.tracks[track_indx].seek_index, cs.tracks[track_indx+1].seek_index)
                 
-            elif track_indx == len(cs.tracks)-1:    
-                cmd = cmd + ' --skip={{{0}}}'.format(cs.tracks[track_indx].seek_index)
                 
-            else:
-                cmd = cmd + ' --skip={{{0}}} --until={{+{1}}}'.format(cs.tracks[track_indx].seek_index, cs.tracks[track_indx+1].seek_index)
-            
-            
-            #cmd = cmd + ' --output-name={0}{1}track{2}.flac {3}'.format(new_f_path.replace(' ', '\ '),os.path.sep,cs.tracks[track_indx].num, cs.audio_file)
-            cmd = cmd + ' --output-name=track{0}.flac {1}'.format(cs.tracks[track_indx].num, cs.audio_file)
-            print(cmd)
-            mf = subprocess.run(cmd, shell=True)
-            print(mf.returncode)
+                if os.path.isfile(os.path.join(new_f_path,'folder.jpg')):
+                    cmd = cmd + ' --picture='+os.path.join(new_f_path.replace(' ', '\ '),'folder.jpg')
+                    
+                    
+                cmd = cmd + ' --output-name={0} {1}'.format(new_file.replace(' ', '\ '), cs.audio_file)
+                print(cmd)
+                mf = subprocess.run(cmd, shell=True)
+                if mf.returncode:
+                    exit("ERROR")
+
             exit("DEBUG")
-    
-        
-        
-        #mf=subprocess.Popen(["flac","--skip={},--until={},-o track.flac ",cs.audio_file],stdout=subprocess.PIPE)
-        #print(mf)
-        #(cues,xxx)=mf.communicate()
-        #if mf.returncode!=0:
-        #    xxx            
 
 if __name__ == "__main__":
     main(sys.argv)
