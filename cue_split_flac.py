e205216af7865418ace8dddf444cde8d14da44ff
josef ling <jling@swcp.com>
2015-12-26 11:22:31 -0700

    Recursive processing from initial input directory

--- cue_split_flac.py
+++ cue_split_flac.py
@@ -5,7 +5,7 @@ Standalone or importable way to split a large flac file by it's cue file
 As often happens when downloading from lazy people on Demonoid
 """
 
-import sys,os,subprocess,re
+import sys,os,subprocess,re,glob
 
 
 
@@ -18,7 +18,7 @@ class track:
         
         self._num         = num
         self._seek_index  = ""
-        self.tags         = {'TRACK':num}
+        self.tags         = {'TRACK':num,'TRACKNUMBER':num}
         
         
     def __str__(self):
@@ -57,7 +57,6 @@ class cuesheet:
         self.cue_file   = cue_file    
         self.f_path     = os.path.split(cue_file)[0]
         self.audio_file = ""
-        #self.cover_image= None
         self.tags       = {}
         self.tracks     = []
         self.t          = track(00)
@@ -112,85 +111,88 @@ class cuesheet:
     def __str__(self):
         return 'CUESHEET:{0}'.format(self.cue_file)
 
+
     def img_parse(self,new_image_out_path):
         """
-        xxx
+        Look for existing cover and provide a 500x500 pixel version into some
+        Directory that can then also be added to the flac tag
         """
         i_file = os.path.join(self.f_path,'cover.jpg')
         if os.path.isfile( i_file ):
             
             from PIL import Image
             i_img = Image.open(i_file)
-            #if i_img.size[0] > 500:
             resize_ratio = 500/i_img.size[0]
             resize_width = int(resize_ratio*i_img.size[0])
             resize_height= int(resize_ratio*i_img.size[1])
             resized_img = i_img.resize( (resize_width, resize_height), Image.ANTIALIAS )
             resized_img.save(os.path.join(new_image_out_path,'folder.jpg'))
-            #self.cover_image = os.path.join(self.f_path,'folder.jpg')
-                
-            #else:
-            #    self.cover_image = default_cover_image
-                
-        
+  
+       
+       
+       
         
 def main(argv):
     
-    i_cue_file = argv[1]
+    # input starting directory; we will traverse this for cue files
+    i_cue_path_top = argv[1]
+    # single output path for where the results will end up
     o_new_files= argv[2]
+
     
+    # glob for all cue file paths. Note: '**', is recursive in python 3.5
+    for i_cue_file in glob.glob(os.path.join(i_cue_path_top,'**','*.cue'), recursive=True):
+        print("\nSplitting: " + i_cue_file + "\n")
+
+        # process each cue file in list
+        if not os.path.isfile(i_cue_file):
+            exit("Cannot file cue file: " + i_cue_file)
+        else:
+            cs = cuesheet(i_cue_file)     
+            cs.cue_parse()
+            
+            
+            new_f_path = os.path.join(o_new_files,cs.tags['ALBUM'] )
+            if not os.path.exists(new_f_path):
+                os.makedirs(new_f_path)
+            
+            cs.img_parse(new_f_path)
+            
+            # process each cue track
+            # loop using an index so we can get data from previous or next track         
+            for track_indx in range(len(cs.tracks)):
+                            
+                # check for existing first 
+                new_file = '{0}{1}track{2}.flac'.format(new_f_path,os.path.sep,cs.tracks[track_indx].num)
+                if not os.path.isfile(new_file):
     
-    if not os.path.isfile(i_cue_file):
-        exit("Cannot file cue file: " + i_cue_file)
-    else:
-        cs = cuesheet(i_cue_file)     
-        cs.cue_parse()
-        
-        
-        new_f_path = os.path.join(o_new_files,cs.tags['ALBUM'] )
-        if not os.path.exists(new_f_path):
-            os.makedirs(new_f_path)
-        
-        cs.img_parse(new_f_path)
-        
-        
-        # TODO: if cover.jpg put that into cuesheet object or just directly into cmdline 
-        # however, i'll first have to check for resolution on that cover.jpg and reduce it to 500
-        # TODO: output file names
-        
-        
-        # process each cue track
-        # loop using an index so we can get data from previous or next track         
-        for track_indx in range(len(cs.tracks)):
+                    # initiate command by dumping out all tags
+                    cmd = 'flac --silent --compression-level-6 ' + ' '.join("--tag={!s}={!r}".format(key,val) for (key,val) in cs.tracks[track_indx].tags.items() )
+                   
+                    # if first track
+                    if track_indx == 0:
+                        cmd = cmd + ' --until={0}'.format(cs.tracks[track_indx+1].seek_index)
                         
-            # check for existing first 
-            new_file = '{0}{1}track{2}.flac'.format(new_f_path,os.path.sep,cs.tracks[track_indx].num)
-            if not os.path.isfile(new_file):
+                    # if last track
+                    elif track_indx == len(cs.tracks)-1:    
+                        cmd = cmd + ' --skip={0}'.format(cs.tracks[track_indx].seek_index)
+                        
+                    else:
+                        cmd = cmd + ' --skip={0} --until={1}'.format(cs.tracks[track_indx].seek_index, cs.tracks[track_indx+1].seek_index)
+                    
+                    
+                    if os.path.isfile(os.path.join(new_f_path,'folder.jpg')):
+                        cmd = cmd + ' --picture='+os.path.join(new_f_path.replace(' ', '\ '),'folder.jpg')
+                        
+                        
+                    cmd = cmd + ' --output-name={0} {1}'.format(new_file.replace(' ', '\ '), cs.audio_file)
+                    print(cmd)
+                    mf = subprocess.run(cmd, shell=True)
+                    if mf.returncode:
+                        exit("ERROR")
+    
 
-                # initiate command by dumping out all tags
-                cmd = 'flac --silent --compression-level-6 ' + ' '.join("--tag={!s}={!r}".format(key,val) for (key,val) in cs.tracks[track_indx].tags.items() )
-               
-                if track_indx == 0:
-                    cmd = cmd + ' --until={0}'.format(cs.tracks[track_indx+1].seek_index)
-                    
-                elif track_indx == len(cs.tracks)-1:    
-                    cmd = cmd + ' --skip={0}'.format(cs.tracks[track_indx].seek_index)
-                    
-                else:
-                    cmd = cmd + ' --skip={0} --until={1}'.format(cs.tracks[track_indx].seek_index, cs.tracks[track_indx+1].seek_index)
-                
-                
-                if os.path.isfile(os.path.join(new_f_path,'folder.jpg')):
-                    cmd = cmd + ' --picture='+os.path.join(new_f_path.replace(' ', '\ '),'folder.jpg')
-                    
-                    
-                cmd = cmd + ' --output-name={0} {1}'.format(new_file.replace(' ', '\ '), cs.audio_file)
-                print(cmd)
-                mf = subprocess.run(cmd, shell=True)
-                if mf.returncode:
-                    exit("ERROR")
 
-            exit("DEBUG")
 
 if __name__ == "__main__":
     main(sys.argv)
