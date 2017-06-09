8e02d8a9c70fa94c56cbe9f3134b9c7fe584b362
josef ling <jling@swcp.com>
2017-06-09 15:31:42 -0600

    Syntax cleanup and some updates for Python 3.6.1

--- cue_split_flac.py
+++ cue_split_flac.py
@@ -2,15 +2,14 @@
 
 """
 Standalone or importable way to split a large flac file by it's cue file
-As often happens when downloading from lazy people on Demonoid
 """
 
-import sys,os,subprocess,re,glob
+import sys, os, subprocess, re, glob, argparse
 
 cover_art_file = 'front.jpg'
 
 
-class track:
+class Track:
     """
     A cue audio track
     """
@@ -47,35 +46,36 @@ class track:
     
     
     
-class cuesheet:
+class CueSheet:
     """
-    cuesheet
+    cuesheet   
     Store cuesheet properties
     """
     
-    def __init__(self,cue_file):
+    def __init__(self, cue_file):
         
         self.cue_file   = cue_file    
         self.f_path     = os.path.split(cue_file)[0]
         self.audio_file = ""
         self.tags       = {}
         self.tracks     = []
-        self.t          = track(00)
+        self.t          = Track(00)
         
     def cue_parse(self):
         """
         Parse text file into cuesheet object
         """
-        
-        with open(self.cue_file) as f:
+
+        # encoding should be utf-16 ???
+        with open(self.cue_file, 'r', encoding='utf-8') as f:
 
             for line in f.readlines():
 
                 if line.startswith('FILE'):
-                    self.audio_file = os.path.join(self.f_path,line.split("\"")[1])
-                    self.audio_file = self.audio_file.replace(" ","\\ ")
-                    self.audio_file = self.audio_file.replace("(","\\(")
-                    self.audio_file = self.audio_file.replace(")","\\)")
+                    self.audio_file = os.path.join(self.f_path, line.split("\"")[1])
+                    self.audio_file = self.audio_file.replace(" ", "\\ ")
+                    self.audio_file = self.audio_file.replace("(", "\\(")
+                    self.audio_file = self.audio_file.replace(")", "\\)")
                                     
                 if line.startswith('REM GENRE'):
                     self.tags['GENRE'] = line.split(' ')[2].strip()
@@ -84,7 +84,7 @@ class cuesheet:
                     self.tags['DATE'] = line.split(' ')[2].strip()
                 
                 if line.startswith('TITLE'):
-                    self.tags['ALBUM'] = re.sub( "[^a-zA-Z0-9 \.]","",line.split('"')[1].strip() )                 
+                    self.tags['ALBUM'] = re.sub("[^a-zA-Z0-9 \.]", "", line.split('"')[1].strip())
                 
 
                 
@@ -92,11 +92,11 @@ class cuesheet:
             for line in f.readlines():
 
                 if "TRACK" in line:
-                    self.t = track(line.split(" ")[3])
+                    self.t = Track(line.split(" ")[3])
                     self.t.tags.update(self.tags)
                                         
                 if "TITLE" in line:
-                    self.t.tags['TITLE'] = line.split('\"')[1].strip().replace('\'','')
+                    self.t.tags['TITLE'] = line.split('\"')[1].strip().replace('\'', '')
                     
                 if "PERFORMER" in line:
                     self.t.tags['ARTIST'] = line.split('"')[1]
@@ -113,47 +113,49 @@ class cuesheet:
         return 'CUESHEET:{0}'.format(self.cue_file)
 
 
-    def img_parse(self,new_image_out_path):
+    def img_parse(self, new_image_out_path):
         """
         Look for existing cover and provide a 500x500 pixel version into some
         Directory that can then also be added to the flac tag
         """
-        i_file = os.path.join(self.f_path,cover_art_file)
-        if os.path.isfile( i_file ):
+        i_file = os.path.join(self.f_path, cover_art_file)
+        if os.path.isfile(i_file):
             
             from PIL import Image
             i_img = Image.open(i_file)
             resize_ratio = 500/i_img.size[0]
             resize_width = int(resize_ratio*i_img.size[0])
             resize_height= int(resize_ratio*i_img.size[1])
-            resized_img = i_img.resize( (resize_width, resize_height), Image.ANTIALIAS )
-            resized_img.save(os.path.join(new_image_out_path,'folder.jpg'))
+            resized_img = i_img.resize((resize_width, resize_height), Image.ANTIALIAS)
+            resized_img.save(os.path.join(new_image_out_path, 'folder.jpg'))
   
        
        
        
         
 def main(argv):
-    
-    # input starting directory; we will traverse this for cue files
-    i_cue_path_top = argv[1]
-    # single output path for where the results will end up
-    o_new_files= argv[2]
+    argparser = argparse.ArgumentParser(description='Split flac file using cue sheet.')
+    argparser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    argparser.add_argument('-id', '--input_dir', metavar='Input Directory', type=str, nargs=1, required=True,
+                           help='Input starting directory; we will traverse this for cue files')
+    argparser.add_argument('-od', '--output_dir', metavar='Output Directory', type=str, nargs=1, required=True,
+                           help='single output path for where the results will end up')
+    args = argparser.parse_args()
 
     
     # glob for all cue file paths. Note: '**', is recursive in python 3.5
-    for i_cue_file in glob.glob(os.path.join(i_cue_path_top,'**','*.cue'), recursive=True):
+    for i_cue_file in glob.glob(os.path.join(args.input_dir[0], '**', '*.cue'), recursive=True):
         print("\nSplitting: " + i_cue_file + "\n")
 
         # process each cue file in list
         if not os.path.isfile(i_cue_file):
             exit("Cannot file cue file: " + i_cue_file)
         else:
-            cs = cuesheet(i_cue_file)     
+            cs = CueSheet(i_cue_file)
             cs.cue_parse()
             
             
-            new_f_path = os.path.join(o_new_files,cs.tags['ALBUM'] )
+            new_f_path = os.path.join(args.output_dir[0], cs.tags['ALBUM'] )
             if not os.path.exists(new_f_path):
                 os.makedirs(new_f_path)
             
@@ -164,11 +166,11 @@ def main(argv):
             for track_indx in range(len(cs.tracks)):
                             
                 # check for existing first 
-                new_file = '{0}{1}track{2}.flac'.format(new_f_path,os.path.sep,cs.tracks[track_indx].num)
+                new_file = '{0}{1}track{2}.flac'.format(new_f_path, os.path.sep, cs.tracks[track_indx].num)
                 if not os.path.isfile(new_file):
     
                     # initiate command by dumping out all tags
-                    cmd = 'flac --silent --compression-level-6 ' + ' '.join("--tag={!s}={!r}".format(key,val) for (key,val) in cs.tracks[track_indx].tags.items() )
+                    cmd = 'flac --silent --compression-level-6 ' + ' '.join("--tag={!s}={!r}".format(key, val) for (key, val) in cs.tracks[track_indx].tags.items())
                    
                     # if first track
                     if track_indx == 0:
@@ -182,8 +184,8 @@ def main(argv):
                         cmd = cmd + ' --skip={0} --until={1}'.format(cs.tracks[track_indx].seek_index, cs.tracks[track_indx+1].seek_index)
                     
                     
-                    if os.path.isfile(os.path.join(new_f_path,'folder.jpg')):
-                        cmd = cmd + ' --picture='+os.path.join(new_f_path.replace(' ', '\ '),'folder.jpg')
+                    if os.path.isfile(os.path.join(new_f_path, 'folder.jpg')):
+                        cmd = cmd + ' --picture='+os.path.join(new_f_path.replace(' ', '\ '), 'folder.jpg')
                         
                         
                     cmd = cmd + ' --output-name={0} {1}'.format(new_file.replace(' ', '\ '), cs.audio_file)
@@ -197,4 +199,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-    
